0cbd546a42802a7f26cb17a7d5d648500100119d
YUVRAJ CHOPRA <yuvrajchopra561@gmail.com>
2025-08-28 15:43:04 +0530

    Create sum of a number in descending order till 1.c

---
+++ sum of a number in descending order till 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,n,sum=0;
+    printf("enter the number:");
+    scanf("%d", &n);
+    for(i=1;i <=n;i++)
+    sum=sum+i;
+    
+    printf( "the sum from 1 to the digit entered by you is:%d", sum);
+
+    return 0;
+}
