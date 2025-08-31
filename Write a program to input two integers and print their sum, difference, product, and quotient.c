5d706bd1a5311bef3db5615717530be53286b391
YUVRAJ CHOPRA <yuvrajchopra561@gmail.com>
2025-08-31 21:25:31 +0530

    Create Write a program to input two integers and print their sum, difference, product, and quotient.c

---
+++ Write a program to input two integers and print their sum, difference, product, and quotient.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    printf("Sum: %d\n", a + b);
+    printf("Difference: %d\n", a - b);
+    printf("Product: %d\n", a * b);
+    printf("Quotient: %d\n", a / b);
+
+    return 0;
+}
