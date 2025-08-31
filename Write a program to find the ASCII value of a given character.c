f99d8de42e5d3e18e53944d911666871434ef99e
YUVRAJ CHOPRA <yuvrajchopra561@gmail.com>
2025-08-31 20:39:00 +0530

    Create Write a program to find the ASCII value of a given character.c

---
+++ Write a program to find the ASCII value of a given character.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    char c;
+    printf("enter the character:");
+    scanf("%c",&c);
+    
+    printf("the ASCII code of the character is %c %d",c,c );
+    return 0;
+}
