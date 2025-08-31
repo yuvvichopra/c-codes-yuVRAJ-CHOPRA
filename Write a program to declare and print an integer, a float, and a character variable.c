51751ad3298e37ba9d70906911136dd7722e26c5
YUVRAJ CHOPRA <yuvrajchopra561@gmail.com>
2025-08-31 19:20:22 +0530

    Create Write a program to declare and print an integer, a float, and a character variable.c

---
+++ Write a program to declare and print an integer, a float, and a character variable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a=30;
+    float b=29.77;
+    char c='U';
+    
+    printf("THE INTEGER IS  %d\n", a);
+    printf("THE FLOAT IS %f\n", b);
+    printf("THE CHARACTER IS %c", c);
+    
+    return 0;
+}
