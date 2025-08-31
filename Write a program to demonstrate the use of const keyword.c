98890017f328c8e39900331928ecc0427d5e0113
YUVRAJ CHOPRA <yuvrajchopra561@gmail.com>
2025-08-31 19:23:12 +0530

    Create Write a program to demonstrate the use of const keyword.c

---
+++ Write a program to demonstrate the use of const keyword.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+   const int a=30;
+   const float b=29.77;
+   const char c='U';
+    
+    printf("THE INTEGER IS  %d\n", a);
+    printf("THE FLOAT IS %f\n", b);
+    printf("THE CHARACTER IS %c", c);
+    
+    return 0;
+}
